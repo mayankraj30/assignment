1f9552ac8c219a4e9c4f6128f63c72bc2665fa00
mayankraj30 <mayankrajaraj@gmail.com>
2018-10-22 02:27:41 -0700

    testfile

---
+++ test.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+ 
+    $.getJSON("category.json",function(data){
+        $.each(data.categories,function(index,category){
+            var subCategoryData="";
+            $.each(category.subcategory,function(index,subcategory){
+                subCategoryData+="<li onClick=\"openProductPage('"+subcategory.name+"')\"><a href=\"#\">"+ subcategory.name+"</a></li>";
+                console.log(subcategory.name);
+            });
+        
+            if(subCategoryData!="")
+            {
+                var dropDownData="<li><a class=\"dropdown-toggle\" data-toggle=\"dropdown\"  href=\"#\">"+category.name+"</a><ul class=\"dropdown-menu\">"+subCategoryData+"</ul></li>"
+            }
+
+            $(dropDownData).appendTo(".menu");
+        
+
+        })
+        //console.log(data);
+    
+    })
+
+
+   
+
+});
+function openProductPage(subcategoryName)
+{
+    document.getElementById("productDisplay").innerHTML="";
+    
+    $.getJSON("products.json",function(data){
+        for(d of data)
+        {
+            console.log(d.category,subcategoryName);
+            if(d.category==subcategoryName)
+            {
+                $("#productDisplay").append(`<div class="row">
+            
+                <div class="column">
+                  <img src="${d.image}" alt="sorry" style="width:100%">
+                </div>
+              </div>`)
+            }
+        }
+
+    });
+}
+
+
