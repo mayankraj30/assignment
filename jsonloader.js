fd6d1bd0a913146875b4fe4f84e2bd306ece0181
mayankraj30 <mayankrajaraj@gmail.com>
2018-10-24 02:21:28 -0700

    dnamic loading of content from json file according to sub category

--- jsonloader.js
+++ jsonloader.js
@@ -1,51 +1,52 @@
-$(document).ready(function(){
- 
-    $.getJSON("category.json",function(data){
-        $.each(data.categories,function(index,category){
-            var subCategoryData="";
-            $.each(category.subcategory,function(index,subcategory){
-                subCategoryData+="<li onClick=\"openProductPage('"+subcategory.name+"')\"><a href=\"#\">"+ subcategory.name+"</a></li>";
+$(document).ready(function () {
+
+    $.getJSON("category.json", function (data) {
+        $.each(data.categories, function (index, category) {
+            var subCategoryData = "";
+            $.each(category.subcategory, function (index, subcategory) {
+                subCategoryData += "<li onClick=\"openProductPage('" + subcategory.name + "')\"><a href=\"#\">" + subcategory.name + "</a></li>";
                 console.log(subcategory.name);
             });
-        
-            if(subCategoryData!="")
-            {
-                var dropDownData="<li><a class=\"dropdown-toggle\" data-toggle=\"dropdown\"  href=\"#\">"+category.name+"</a><ul class=\"dropdown-menu\">"+subCategoryData+"</ul></li>"
+
+            if (subCategoryData != "") {
+                var dropDownData = "<li><a class=\"dropdown-toggle\" data-toggle=\"dropdown\"  href=\"#\">" + category.name + "</a><ul class=\"dropdown-menu\">" + subCategoryData + "</ul></li>"
             }
 
             $(dropDownData).appendTo(".menu");
-        
+
 
         })
         //console.log(data);
-    
+
     })
 
 
-   
+
 
 });
-function openProductPage(subcategoryName)
-{
-    document.getElementById("productDisplay").innerHTML="";
-    
-    $.getJSON("products.json",function(data){
-        for(d of data)
-        {
-            console.log(d.category,subcategoryName);
-            if(d.category==subcategoryName)
-            {
-                $("#productDisplay").append(`<div class="row">
+function openProductPage(subcategoryName) {
+    document.getElementById("productDisplay").innerHTML = "";
+
+    $.getJSON("products.json", function (data) {
+        for (d of data) {
+            console.log(d.category, subcategoryName);
+            if (d.category == subcategoryName) {
+                $("#productDisplay").append(`<div class="col-md-4 col-sm-6 col-xs-12">
             
                 <div class="column">
                   <img src="${d.image}" alt="sorry" style="width:100%">
+                  <div class="btn btn-primary">add to cart</div>
                 </div>
-                <div class="btn btn-primary">add to cart</div>
+                
               </div>`)
             }
         }
 
     });
 }
+
+
+
+
 
 
